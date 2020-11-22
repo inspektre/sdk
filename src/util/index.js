d9312289ca9b4a1449f72f31cca7db664b867692
Uday Korlimarla <uday@inspektre.io>
2020-11-22 14:23:59 +1100

    add date instead of day

--- src/util/index.js
+++ src/util/index.js
@@ -27,11 +27,11 @@ const generateDate = (dateString) => {
     const date = new Date(dateString);
     return {
         year: date.getFullYear(),
-        month: date.getMonth(),
-        day: date.getDay(),
-        hour: date.getUTCHours(),
-        minute: date.getUTCMinutes(),
-        second: date.getUTCSeconds()
+        month: date.getMonth()+1,
+        day: date.getDate(),
+        hour: date.getHours(),
+        minute: date.getMinutes(),
+        second: date.getSeconds()
     }
 }
 // Create authorization Link middleware
