581999369ef5a88f08097a105f70c81308d9eac8
Uday Korlimarla <uday@inspektre.io>
2021-02-27 10:17:10 +1100

    cleanup postprocessing

--- src/inspect/index.js
+++ src/inspect/index.js
@@ -1,69 +1,17 @@
 const { generateMeta } = require('./generateMeta');
-const { generateDate } = require('../util');
-const {
-    setProjectCodeIntel,
-    alterProjectUpdated,
-    createProject,
-    createScans,
-    setVerificationsMeta,
-    setAttacksMeta,
-    setWeaknessMeta,
-    setScansMeta,
-    setProjectCodeIntelMeta,
-    setSarifProjectMeta,
-    setSarifAttacksMeta,
-    setCodeIntelScansMeta,
-    setCodeIntelAttacksMeta
-} = require('../mutations');
 const { projectExists } = require('../queries');
-const { consumeDCAISarif } = require('../sarif');
-const chalk = require('chalk');
-const figures = require('figures');
+const { postProcessing } = require('./postProcessing');
+
+
 
 const inspect =  async (project, data, checkSarif, sarif) => {
-    // Step #1: Generate Metadata and a Project
+    // Generate Metadata and add to existing project
     const meta = generateMeta(project, data);
     const existingProject = await projectExists(meta.projectName);
     let projectId = null;
-    if(existingProject) {
+    if(existingProject && existingProject.projectId) {
         projectId = existingProject.projectId;
-        // Update Time of change for existing proj.
-        // To-Do: Combine these two mutations into one
-        /* 
-            Temporary patch for ISO String - GQL or APOC Bug
-        */
-        await alterProjectUpdated(meta.projectName, existingProject.projectId, generateDate(new Date().toISOString()));
-   
-    
-        // /* Step #2: Record Scan Results */
-        const codeIntelEntry = await setProjectCodeIntel(meta);
-        await setProjectCodeIntelMeta(meta.projectName, meta.version, codeIntelEntry);
-        const scanRecords = await Promise.all(meta.repoResults.map(result => createScans(result)));
-
-        // /* Step #3: Security Graphs */
-        if(scanRecords) {
-            const prjktScanMeta = await Promise.all(scanRecords.map(scanId => setScansMeta(meta.projectName, meta.version, scanId)));
-            if(prjktScanMeta) {
-                console.log('Project Scans Meta is set.');
-            }
-            
-        const codeIntlScanMeta = await Promise.all(scanRecords.map(scanId => setCodeIntelScansMeta(codeIntelEntry, meta.projectName, meta.version, scanId)));
-        if(codeIntlScanMeta) {
-            console.log('CodeIntel Scans Meta is set.');
-        }
-        }
-        if(checkSarif) {
-            const sarifEntry = await consumeDCAISarif(sarif, meta.projectName, meta.version);
-            await setSarifProjectMeta(sarifEntry, meta.projectName, meta.version);
-            await setSarifAttacksMeta(meta.projectName, sarifEntry);
-        }
-        
-        await setVerificationsMeta(meta.projectName);
-        await setWeaknessMeta(meta.projectName);
-        await setAttacksMeta(meta.projectName);
-        await setCodeIntelAttacksMeta(meta.projectName, codeIntelEntry);
-        // // SARIF - Projects - Attacks Meta
-        // process.stdout.write('Security Graphs are being generated. All tasks are complete.\n');
+        await postProcessing(meta, projectId, checkSarif, sarif);
     }
     else {
         process.stderr.write(chalk.red(figures.main.cross).concat(`${meta.projectName} does not exist.!\n`));
@@ -71,8 +19,6 @@ const inspect =  async (project, data, checkSarif, sarif) => {
             process.exit(-1);
         }, 1500);
     }
-    
-    
 };
 
 module.exports = {
---
+++ src/inspect/postProcessing.js
@@ -0,0 +1,64 @@
+const chalk = require('chalk');
+const figures = require('figures');
+const { generateDate } = require('../util');
+const {
+  setProjectCodeIntel,
+  alterProjectUpdated,
+  createProject,
+  createScans,
+  setVerificationsMeta,
+  setAttacksMeta,
+  setWeaknessMeta,
+  setScansMeta,
+  setProjectCodeIntelMeta,
+  setSarifProjectMeta,
+  setSarifAttacksMeta,
+  setCodeIntelScansMeta,
+  setCodeIntelAttacksMeta
+} = require('../mutations');
+const { consumeDCAISarif } = require('../sarif');
+
+
+const postProcessing = async (meta, projectId, checkSarif, sarif) => {
+          // Update Time of change for existing proj.
+        // To-Do: Combine these two mutations into one
+        /* 
+            Temporary patch for ISO String - GQL or APOC Bug
+        */
+       await alterProjectUpdated(meta.projectName, projectId, generateDate(new Date().toISOString()));
+   
+    
+       // /* Step #2: Record Scan Results */
+       const codeIntelEntry = await setProjectCodeIntel(meta);
+       await setProjectCodeIntelMeta(meta.projectName, meta.version, codeIntelEntry);
+       const scanRecords = await Promise.all(meta.repoResults.map(result => createScans(result)));
+
+       // /* Step #3: Security Graphs */
+       if(scanRecords) {
+           const prjktScanMeta = await Promise.all(scanRecords.map(scanId => setScansMeta(meta.projectName, meta.version, scanId)));
+           if(prjktScanMeta) {
+               console.log('Project Scans Meta is set.');
+           }
+           
+       const codeIntlScanMeta = await Promise.all(scanRecords.map(scanId => setCodeIntelScansMeta(codeIntelEntry, meta.projectName, meta.version, scanId)));
+       if(codeIntlScanMeta) {
+           console.log('CodeIntel Scans Meta is set.');
+       }
+       }
+       if(checkSarif) {
+           const sarifEntry = await consumeDCAISarif(sarif, meta.projectName, meta.version);
+           await setSarifProjectMeta(sarifEntry, meta.projectName, meta.version);
+           await setSarifAttacksMeta(meta.projectName, sarifEntry);
+       }
+       
+       await setVerificationsMeta(meta.projectName);
+       await setWeaknessMeta(meta.projectName);
+       await setAttacksMeta(meta.projectName);
+       await setCodeIntelAttacksMeta(meta.projectName, codeIntelEntry);
+       // // SARIF - Projects - Attacks Meta
+       // process.stdout.write('Security Graphs are being generated. All tasks are complete.\n');
+}
+
+module.exports = {
+  postProcessing
+}
